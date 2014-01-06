e02a9cea468d1e3da365964d1b974e152bf1bdc5
Matthew Bender <benderm@gmail.com>
2014-01-05 22:41:38 -0700

    fix SECRET_KEY_BASE warning intitalizer

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,8 +9,8 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-if Rails.env.production? && ENV['SECRET_TOKEN'].blank?
-  raise 'SECRET_TOKEN variable must be set!'
+if Rails.env.production? && ENV['SECRET_KEY_BASE'].blank?
+  raise 'SECRET_KEY_BASE variable must be set!'
 end
 
 Code5280::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
